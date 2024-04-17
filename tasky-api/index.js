7739c90cf7b72090bec12464af4186fd19f7383d
ConorMcCarthy1 <90519422+ConorMcCarthy1@users.noreply.github.com>
2024-04-17 13:15:39 +0100

    Updated stuff

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import express from 'express';
-import tasksRouter from './api/tasks';
+import tasksRouter from './api/tasks';  
+import usersRouter from './api/users';
+import './db';
 
 dotenv.config();
 
@@ -8,8 +10,18 @@ const app = express();
 
 const port = process.env.PORT;
 
+const errHandler = (err, req, res, next) => {
+     /* if the error in development then send stack trace to display whole error,
+     if it's in production then just send error message  */
+     if(process.env.NODE_ENV === 'production') {
+       return res.status(500).send(`Something went wrong!`);
+     }
+     res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+   };
 app.use(express.json());
-app.use('/api/tasks', tasksRouter);
+app.use('/api/tasks', tasksRouter); 
+app.use('/api/users', usersRouter); 
+app.use(errHandler);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
